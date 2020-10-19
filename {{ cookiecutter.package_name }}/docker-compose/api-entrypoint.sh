14e467ba183a429e49bc8bda666fecc305171c61
CrossNox <ijmermet@gmail.com>
2020-10-19 18:00:04 -0300

    forgot to parametrize this

--- {{ cookiecutter.package_name }}/docker-compose/api-entrypoint.sh
+++ {{ cookiecutter.package_name }}/docker-compose/api-entrypoint.sh
@@ -2,8 +2,8 @@
 docker-compose/wait-for-postgres.sh
 
 echo 'creating db'
-poetry run python cctest2heroku/manage.py db_create
+poetry run python {{cookiecutter.package_name}}/manage.py db_create
 echo 'upgrading db'
-poetry run python cctest2heroku/manage.py db upgrade
+poetry run python {{cookiecutter.package_name}}/manage.py db upgrade
 echo 'starting server'
-poetry run gunicorn -w 2 --bind 0.0.0.0:5000 "cctest2heroku.app:create_app()"
+poetry run gunicorn -w 2 --bind 0.0.0.0:5000 "{{cookiecutter.package_name}}.app:create_app()"
