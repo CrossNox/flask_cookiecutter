74447a4303c1ef52ad677c32b83b2b2ea9594467
CrossNox <ijmermet@gmail.com>
2021-03-01 14:15:36 -0300

    fix

--- {{ cookiecutter.package_name }}/heroku/heroku-entrypoint.sh
+++ {{ cookiecutter.package_name }}/heroku/heroku-entrypoint.sh
@@ -3,4 +3,4 @@
 datadog-agent run > /dev/null &
 /opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
 /opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
-poetry run gunicorn --log-level=debug --bind 0.0.0.0:$PORT "test_dd.app:create_app()"
+poetry run gunicorn --log-level=debug --bind 0.0.0.0:$PORT "{{cookiecutter.package_name}}.app:create_app()"
